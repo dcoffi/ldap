44fa8ce135d954d19dbbb99f59a233bb8e4a8447
Daivinson Coffi <dcoffi@PR400429l.intra.bcv>
2017-10-25 08:50:54 -0400

    commit Creado proyecto ldap

---
+++ ScrapyCoffi/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+# Create your views here.
+from ldap3 import Server, Connection, ALL, Tls
+import ssl
+from django.contrib.auth import authenticate, login
+
+@login_required
+def mostrarusuarioldap(request):
+    usuario="francys"
+    tls_configuration = Tls( validate=ssl.CERT_REQUIRED, version=ssl.PROTOCOL_TLSv1 )
+    tls_configuration.validate = ssl.CERT_NONE
+    server = Server( 'ldaps://172.24.3.3:636', use_ssl=True, tls=tls_configuration )
+    conn = Connection( server, 'cn=dcoffi,ou=dtsc,ou=admin,ou=ccs,o=bcv', 'jeanandry28', auto_bind=True )
+    conn.search( 'O=BCV', '(fullname=' + usuario + '*)',
+                 attributes=['fullName', 'telephoneNumber', 'title', 'workforceID', 'l', 'mail', 'ou',
+                             'passwordExpirationTime'] )
+    valor = conn.entries
+    ctx={'usuarios' : valor}
+    return render(request=request, context=ctx, template_name='usuarios.html')
+#def milogin(request):
